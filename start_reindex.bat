795647ea0d0177eceecf4bd7e725a45feccbcbfc
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2022-10-27 04:03:32 +0700

    Update stable and preview build to 1.0.52.5
    Former-commit-id: 0ec6b47a4d9952c85c5d90eaabcbaa510a743a82

---
+++ start_reindex.bat
@@ -0,0 +1,9 @@
+@echo off
+%~d0
+cd "%~dp0"
+set ver=1.0.52.5
+
+preview\CollapseLauncher.exe reindex --input preview\ --upver %ver%
+previewportable\CollapseLauncher.exe reindex --input previewportable\ --upver %ver%
+stable\CollapseLauncher.exe reindex --input stable\ --upver %ver%
+stableportable\CollapseLauncher.exe reindex --input stableportable\ --upver %ver%
