175e4561b5ae8aed7014ca8f281d314b216c1a07
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2023-02-06 00:45:04 +0700

    Update preview release to 1.0.64.0
    Former-commit-id: 3e4dbd33e9e34c60e5265a2b476b6fa4c9a7d4cf [formerly e6f529ddfcd1038709ec08f3f3542f0cdc0787fa] [formerly 8e2b7304de46a651c184e899a925d0919d55d8ec [formerly 8e2b7304de46a651c184e899a925d0919d55d8ec [formerly 8df1e87fd0da9c18c2d0e9a5151255ff076d42e5]]]
    Former-commit-id: 0336328f20228090cd25e7b3e6bef79c4224363a [formerly 0336328f20228090cd25e7b3e6bef79c4224363a [formerly fc5837f34ccb49990768c6ec585b5fb538baa914]]
    Former-commit-id: d6d2fef00a1e16f8b5eb668e8a656fb8d40b9919
    Former-commit-id: 0bd3cb8037b34951bcf1389d19199fee28e1617c
    Former-commit-id: 2f19d1d9920f0a4e6738b65aef23e7947512eb75

--- start_reindex.bat
+++ start_reindex.bat
@@ -1,9 +1,9 @@
 @echo off
 %~d0
 cd "%~dp0"
-set ver=1.0.55.6
+set ver=1.0.64.0
 
 preview\CollapseLauncher.exe reindex --input preview\ --upver %ver%
 previewportable\CollapseLauncher.exe reindex --input previewportable\ --upver %ver%
-stable\CollapseLauncher.exe reindex --input stable\ --upver %ver%
-stableportable\CollapseLauncher.exe reindex --input stableportable\ --upver %ver%
+:: stable\CollapseLauncher.exe reindex --input stable\ --upver %ver%
+:: stableportable\CollapseLauncher.exe reindex --input stableportable\ --upver %ver%
