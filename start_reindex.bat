8050bb450f201e3de37d26b2d38e73a1a56e4a66
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2023-02-22 01:42:59 +0700

    Update preview build to 1.0.69.2
    Former-commit-id: ee6752308e9f46be6cc279ac6a0c1c97352cfb53 [formerly ec182e219b1c164c686fb6908cf3d0d8690d0281] [formerly d208c29e3f64970f18ee7a579f09f1a732d66c9a [formerly d208c29e3f64970f18ee7a579f09f1a732d66c9a [formerly feabdb18a416f0a3a4daffb6377b6ac3a070deab]]]
    Former-commit-id: 2bf354f3ee6b94bbea56194dabd5f49f89d67945 [formerly 2bf354f3ee6b94bbea56194dabd5f49f89d67945 [formerly 3e3c4999d380932e56f597baa0972d000df61604]]
    Former-commit-id: de934009a59cd0194be64f5227c144bc33e5535a
    Former-commit-id: d8d475e843b77cd787a0afbde4c06310dfa6f5c8

--- start_reindex.bat
+++ start_reindex.bat
@@ -1,7 +1,7 @@
 @echo off
 %~d0
 cd "%~dp0"
-set ver=1.0.69.1
+set ver=1.0.69.2
 
 preview\CollapseLauncher.exe reindex --input preview\ --upver %ver%
 previewportable\CollapseLauncher.exe reindex --input previewportable\ --upver %ver%
