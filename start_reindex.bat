be9df995a6f7e840d2a2b5c5e304f44598bf2c4c
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2023-03-01 22:29:01 +0700

    Update preview build to 1.0.69.4
    Former-commit-id: f33cc65c6f8ef8da13737492db6456543b1a6577

--- start_reindex.bat
+++ start_reindex.bat
@@ -1,7 +1,7 @@
 @echo off
 %~d0
 cd "%~dp0"
-set ver=1.0.69.3
+set ver=1.0.69.4
 
 preview\CollapseLauncher.exe reindex --input preview\ --upver %ver%
 previewportable\CollapseLauncher.exe reindex --input previewportable\ --upver %ver%
