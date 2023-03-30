2a0e29273441bb4c335b2ac88a8e3044fece35c7
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2023-03-31 01:45:39 +0700

    Add squirrel update switch-out payload for preview build
    Former-commit-id: 3e562ed65c6620972320d06014bd82b0be8cd061

--- preview/Misc/InstallMediaPack.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-echo Please do not close this console window until it finishes
-powershell.exe -ExecutionPolicy Bypass -Command "Get-WindowsCapability -Online | Where-Object -Property Name -Like "*Media.MediaFeaturePack*" | Add-WindowsCapability -Online"
