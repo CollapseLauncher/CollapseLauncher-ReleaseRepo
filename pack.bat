50c469daa4ff98aee1c3e77a13dbe2247973daa0
Ron Friedman <cryogen65@gmail.com>
2023-07-19 07:29:25 +0200

    1.71.3 & 1.70.12
    :prayge:
    
    
    Former-commit-id: 7d27e988c79062b9a858254a3146c6c5421d05fe [formerly 3d3be1fe2b958cf3196788d31f401a94bfd7b7da]
    Former-commit-id: 96ab3068ed65a71fd3990d3500c9b698c782fa86
    Former-commit-id: f168afc12f8dd578b0204f0069a3324dd0e7ae1b

--- pack.bat
+++ pack.bat
@@ -3,7 +3,7 @@ set _7zFast="C:\Program Files\7-Zip-Zstandard\7z.exe"
 set _Inno="C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
 set _7z="C:\Program Files\7-Zip\7z.exe"
 set name=Collapse
-set version=1.70.9
+set version=1.70.12
 set channel=1
 
 :checkRepoLocation
