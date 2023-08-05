c69cf9bd40b7777ccf6a03aabb7c2d36396710dc
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2023-08-05 18:40:55 +0700

    Update builds to 1.70.14 stable and 1.71.5 preview

--- start_reindex.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-%~d0
-cd "%~dp0"
-set ver=1.0.69.4
-
-preview\CollapseLauncher.exe reindex --input preview\ --upver %ver%
-previewportable\CollapseLauncher.exe reindex --input previewportable\ --upver %ver%
-:: stable\CollapseLauncher.exe reindex --input stable\ --upver %ver%
-:: stableportable\CollapseLauncher.exe reindex --input stableportable\ --upver %ver%
