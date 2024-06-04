d04be7ae3321678db6c3f37899ea02cb0f7a2708
Ron Friedman <cryogen65@gmail.com>
2024-06-03 21:54:11 -0400

    CL-1.80.7
    Former-commit-id: bb7540dc6a626d1794af58511f21fb8592096b20

--- pack_signed.bat
+++ pack_signed.bat
@@ -3,7 +3,7 @@ set _7zFast="C:\Program Files\7-Zip-Zstandard\7z.exe"
 set _7z="C:\Program Files\7-Zip\7z.exe"
 set name=Collapse
 set channel=1
-set version=1.80.1
+set version=1.80.7
 set thread=%NUMBER_OF_PROCESSORS%
 set forceUpdate="forceUpdate":false,
 
